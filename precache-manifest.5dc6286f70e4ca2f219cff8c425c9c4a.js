b8a051aea478c83b4351be22a0a61a55e456402d
maleriepace <malerie.kaplan@gmail.com>
2020-08-24 07:02:51 -0500

    v1.1 build

---
+++ precache-manifest.5dc6286f70e4ca2f219cff8c425c9c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52611a4d26e34e3b61befdbb1f36c58c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f0b6d4d7f9f8bf24587",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff3b66308c6089ab349b",
+    "url": "./static/js/2.50c472b6.chunk.js"
+  },
+  {
+    "revision": "0392075761e21e7b0498de38da80d0aa",
+    "url": "./static/js/2.50c472b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0b6d4d7f9f8bf24587",
+    "url": "./static/js/main.cfc86fcf.chunk.js"
+  },
+  {
+    "revision": "3b65ef155620dd630a0e",
+    "url": "./static/js/runtime-main.61a671e1.js"
+  }
+]);
